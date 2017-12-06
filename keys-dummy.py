36643bb0f433146b272e62537b4b3f2e59323492
farewelltolurking <rik.wassens@gmail.com>
2017-12-06 23:38:12 +0100

    Added dummy keys for easier cloning

---
+++ keys-dummy.py
@@ -0,0 +1,10 @@
+# register your app and get your keys here: https://apps.twitter.com/
+
+def retrieveKeys():
+	data = {
+		'APP_KEY' : 'YOUR APP KEY',
+		'APP_SECRET' : 'YOUR APP SECRET',
+		'ACCESS_TOKEN' : 'YOUR ACCESS TOKEN',
+		'ACCESS_TOKEN_SECRET':'YOUR SECRET ACCESS TOKEN'
+		}
+	return data
