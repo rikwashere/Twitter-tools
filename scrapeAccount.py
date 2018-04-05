74256fd5d8a1cb614858ccd0bdc92498ac26be2e
Rik Wassens <me@rikwassens.com>
2018-04-05 10:03:00 +0200

    Finally stopped loop

--- scrapeAccount.py
+++ scrapeAccount.py
@@ -20,20 +20,27 @@ def crawlAccount(target):
 
 
 	# stop this loop
-	while len(set(ids)) == len(ids):
+	while len(ids) < user[0]['statuses_count']:
+		if len(user_timeline) == 0:
+			print '[!] No more tweets available. Ending scraper.\n'
+			break
+
 		for tweet in user_timeline:
 			ids.append(tweet['id'])			
 			tweets.append(tweet)
-			with open('data/%s-tweets.json' % target, 'a') as json_out:
+
+			with open('../Raw data/tweets/%s.json' % screen_name, 'a') as json_out:
 				json.dump(tweet, json_out)
 				json_out.write('\n')
 
-		print 'Found %i tweets so far. %i unique.' % (len(ids), len(set(ids)))
+		print '\t[i] Found %i tweets so far.' % (len(ids))
 		
 		time.sleep(5)
-		user_timeline = twitter.get_user_timeline(screen_name=target, count=200, max_id=min(ids) - 1, include_rts=True, exclude_replies=False)	
+		user_timeline = twitter.get_user_timeline(screen_name=screen_name, count=200, max_id=min(ids) - 1, include_rts=True, exclude_replies=False)	
+		
 	else:
-		sys.exit('No new tweets found. Ending scraper.')
+		print '[!] All tweets scraped. Ending scraper.\n'
+		return
 	
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
